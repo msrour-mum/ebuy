828cf9d39aef2761dd6336a7642a2eaaf07729dd
Adeola Adeleke <apps@simplyswift.com>
2020-08-01 14:58:55 -0500

    added admin manage-product-list, category-list, shopping-product-list

--- web/src/app/modules/admin/admin.module.ts
+++ web/src/app/modules/admin/admin.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ManageProductListComponent } from './manage-product-list/manage-product-list.component';
+import { ManageCategoryListComponent } from './manage-category-list/manage-category-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ManageProductListComponent, ManageCategoryListComponent],
   imports: [
     CommonModule
   ]
