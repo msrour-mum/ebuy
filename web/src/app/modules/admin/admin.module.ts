9e3eca25df94a8de1fccdfa589a1d15fa20066f1
Adeola Adeleke <apps@simplyswift.com>
2020-08-01 17:12:48 -0500

    added app-product

--- web/src/app/modules/admin/admin.module.ts
+++ web/src/app/modules/admin/admin.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ManageProductListComponent } from './manage-product-list/manage-product-list.component';
-import { ManageCategoryListComponent } from './manage-category-list/manage-category-list.component';
+import { ManageProductListComponent } from './manage-product/manage-product-list.component';
+import { ManageCategoryListComponent } from './manage-category/manage-category-list.component';
+import { AddCategoryComponent } from './manage-category/add-category/add-category.component';
+import { AddProductComponent } from './manage-product/add-product/add-product.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [ManageProductListComponent, ManageCategoryListComponent],
+  declarations: [ManageProductListComponent, ManageCategoryListComponent, AddCategoryComponent, AddProductComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
