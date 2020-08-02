03ceb7f3c907eb0cafc719f8f410bbcb04851247
Moustafa Zein <mzein@miu.edu>
2020-08-01 21:13:17 -0500

    Model 4

--- server/src/main/java/edu/miu/ebuy/models/dto/OrdersDto.java
+++ server/src/main/java/edu/miu/ebuy/models/dto/OrdersDto.java
@@ -1,4 +1,37 @@
 package edu.miu.ebuy.models.dto;
 
+import edu.miu.ebuy.models.OrderItems;
+import edu.miu.ebuy.models.Orders;
+import edu.miu.ebuy.models.User;
+
+import javax.persistence.*;
+import java.util.Date;
+
 public class OrdersDto {
+
+    private long id;
+    private String  userName;
+    private Date orderDate;
+    private double total;
+    private double shipping;
+    private double netTotal;
+
+    public OrdersDto(long id, String userName, Date orderDate, double total, double shipping) {
+        this.id = id;
+        this.userName = userName;
+        this.orderDate = orderDate;
+        this.total = total;
+        this.shipping = shipping;
+        this.netTotal =  total+shipping;
+    }
+
+
+    public static OrdersDto read(Orders or)
+    {
+        OrdersDto ordersDto = new OrdersDto(or.getId(),or.getUser().getName(),or.getOrderDate(),or.getTotal(),or.getShipping());
+
+
+
+        return ordersDto;
+    }
 }
--- server/src/main/java/edu/miu/ebuy/models/dto/OrdersItemsDto.java
+++ server/src/main/java/edu/miu/ebuy/models/dto/OrdersItemsDto.java
@@ -1,4 +1,47 @@
 package edu.miu.ebuy.models.dto;
 
+import edu.miu.ebuy.models.OrderItems;
+import edu.miu.ebuy.models.Product;
+
+import javax.persistence.*;
+import java.util.Date;
+
 public class OrdersItemsDto {
+
+    private long id;
+
+//    private long orderId;
+//    private String  orderUserName;
+//    private Date orderDate;
+//    private double orderTotal;
+//    private double orderShipping;
+//    private double orderNetTotal;
+
+    private String  productName;
+    private double  productPrice;
+    private double  productCost;
+    private String  productVendor;
+    private String  productDescription;
+
+
+    private int quantity;
+    private double itemTotal;
+
+    private double ItemProfit;
+
+    public static OrdersItemsDto read(OrderItems item)
+    {
+        OrdersItemsDto o = new OrdersItemsDto();
+        o.id=item.getId();
+        o.ItemProfit=item.getQuantity() * ( item.getProduct().getPrice()-item.getProduct().getCost());
+        o.itemTotal=item.getQuantity() * item.getProduct().getPrice();
+        o.productCost=item.getProduct().getCost();
+        o.productDescription=item.getProduct().getDescription();
+        o.productName=item.getProduct().getName();
+        o.productPrice=item.getProduct().getPrice();
+        o.productVendor=item.getProduct().getUser().getName();
+        o.quantity=item.getQuantity();
+        return o;
+    }
+
 }
