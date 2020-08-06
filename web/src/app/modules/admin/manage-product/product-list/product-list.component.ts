9ffa7f9d0ec3cc953bdd6f0909cb3e851c941189
Mamoud Srour <msrour@mum.edu>
2020-08-05 19:21:33 -0500

    Add search on Admin/Product-list page

--- web/src/app/modules/admin/manage-product/product-list/product-list.component.ts
+++ web/src/app/modules/admin/manage-product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-manage-product-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  public searchForm: FormGroup;
+
+  constructor(private  fb: FormBuilder) {
+  this.searchForm = this.fb.group(
+      {
+        search: [''],
+      });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(): void {
+    // let search = this.searchForm.get('search').value;
+    // this.searchForm.reset();
+    // this.router.navigate(['search'], {queryParams: {q: search}});
   }
 
 }
